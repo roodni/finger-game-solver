6b3835479ef4a2e0c0108a19e7ef930bbed5cbe8
rodoni <41773436+roodni@users.noreply.github.com>
2019-11-01 20:33:23 +0900

    modの実装を変更 modルール以外に対応しやすくした

--- src/GameState.cpp
+++ src/GameState.cpp
@@ -3,19 +3,26 @@
 #include <algorithm>
 #include <tuple>
 
-// MODルール
-const int mod = 5;
+namespace {
+    const int max = 4;  // 指の本数
+}
 
 GameState::GameState(int player, int p1, int p2, int e1, int e2) {
+    const int mod = max + 1;
+    p1 %= mod;
+    p2 %= mod;
+    e1 %= mod;
+    e2 %= mod;
+
     // l <= r となるようにする
     l[player] = std::min(p1, p2);
     r[player] = std::max(p1, p2);
-    assert(0 <= l[player] && r[player] < mod);
+    assert(0 <= l[player] && r[player] <= max);
 
     int enemy = 1 - player;
     l[enemy] = std::min(e1, e2);
     r[enemy] = std::max(e1, e2);
-    assert(0 <= l[enemy] && r[enemy] < mod);
+    assert(0 <= l[enemy] && r[enemy] <= max);
 
     assert(player == 0 || player == 1);
     this->player = player;
@@ -44,35 +51,35 @@ void GameState::addTransSet(std::set<GameState> &set) const {
     if (pl != 0) {
         // 相手左を攻撃
         if (el != 0) {
-            set.insert(GameState(enemy, (el + pl) % mod, er, pl, pr));
+            set.insert(GameState(enemy, el + pl, er, pl, pr));
         }
         // 相手右を攻撃
         if (er != 0) {
-            set.insert(GameState(enemy, el, (er + pl) % mod, pl, pr));
+            set.insert(GameState(enemy, el, er + pl, pl, pr));
         }
         // 自分右を攻撃
         if (pr != 0) {
-            set.insert(GameState(enemy, el, er, pl, (pr + pl) % mod));
+            set.insert(GameState(enemy, el, er, pl, pr + pl));
         }
     }
     // 自分右で
     if (pr != 0) {
         // 相手左を攻撃
         if (el != 0) {
-            set.insert(GameState(enemy, (el + pr) % mod, er, pl, pr));
+            set.insert(GameState(enemy, el + pr, er, pl, pr));
         }
         // 相手右を攻撃
         if (er != 0) {
-            set.insert(GameState(enemy, el, (er + pr) % mod, pl, pr));
+            set.insert(GameState(enemy, el, er + pr, pl, pr));
         }
         // 自分左を攻撃
         if (pl != 0) {
-            set.insert(GameState(enemy, el, er, (pl + pr) % mod, pr));
+            set.insert(GameState(enemy, el, er, pl + pr, pr));
         }
     }
     // 分身
-    int lmin = std::max(pl + pr - (mod - 1), 0);
-    int lmax = std::min(pl + pr, mod - 1);
+    int lmin = std::max(pl + pr - max, 0);
+    int lmax = std::min(pl + pr, max);
     for (int i = lmin; i <= lmax; i++) {
         if (i == pl) continue;
         int nl = i;
