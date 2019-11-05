de19ffe06193bc97348466ee16ced45477ecd0a9
rodoni <41773436+roodni@users.noreply.github.com>
2019-11-05 18:24:07 +0900

    入力のエラーを弾く 出力を手続き化、標準出力に出力したファイル名一覧を吐く

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,9 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <tuple>
+#include <functional>
 
-void unko(const std::string &str) {
-    std::cout << str << std::endl;
-}
+void fileOutput(std::string fileName, std::function<void(std::ostream &)> toStream);
 
 int main() {
     int max;
@@ -18,143 +16,170 @@ int main() {
     GraphLoop graphLoop;
     GraphShowLose graphShowLose;
 
-    // 設定を入力
-    std::cout
+    std::string prefix;  // ファイル名のプレフィックス
+    std::string suffix;  // ファイル名のサフィックス
+
+    // ルール設定を入力
+    std::cerr
         << "[ルール設定]\n"
-        << "* 指の本数の最大数\n"
-        << "> ";
-    std::cin >> max;
-    std::cout
+        << "* 指の本数の最大数\n";
+    do {
+        std::cerr << "> ";
+        std::cin >> max;
+    } while (max <= 0);
+    prefix += std::to_string(max);
+    prefix += "_";
+
+    std::cerr
         << "\n"
-        << "オーバーフロー時の動作:\n"
-        << "0: mod " << (max + 1) << "\n"
-        << "1: ゼロ\n"
-        << "> ";
-    std::cin >> num;
-    overflow = static_cast<RuleOverflow>(num);
-    std::cout
+        << "* 指の本数のオーバーフロー\n"
+        << "0: 0\n"
+        << "1: mod " << (max + 1) << "\n";
+    do {
+        std::cerr << "> ";
+        std::cin >> num;
+        switch (num) {
+        case 0:
+            prefix += "no-mod";
+            overflow = RuleOverflow::death;
+            break;
+        case 1:
+            prefix += "mod";
+            overflow = RuleOverflow::mod;
+            break;
+        default:
+            num = -1;
+            break;
+        }
+    } while (num == -1);
+    prefix += "_";
+
+    std::cerr
         << "\n"
         << "* 分身と結合\n"
-        << "0: 常に可能\n"
-        << "1: 片方が0のときのみ可能\n"
-        << "2: 禁止\n"
-        << "> ";
-    std::cin >> num;
-    bunshin = static_cast<RuleBunshin>(num);
-    std::cout
+        << "0: 禁止\n"
+        << "1: 常に可能\n"
+        << "2: 片方が0のときのみ可能\n";
+    do {
+        std::cerr << "> ";
+        std::cin >> num;
+        switch (num) {
+        case 0:
+            prefix += "no-split";
+            bunshin = RuleBunshin::forbid;
+            break;
+        case 1:
+            prefix += "split";
+            bunshin = RuleBunshin::allow;
+            break;
+        case 2:
+            prefix += "resuscitation-only-split";
+            bunshin = RuleBunshin::resuscitateOnly;
+            break;
+        default:
+            num = -1;
+            break;
+        }
+    } while (num == -1);
+    prefix += "_";
+
+    std::cerr
         << "\n"
         << "* 自分への攻撃\n"
-        << "0: 可能\n"
-        << "1: 禁止\n"
-        << "> ";
-    std::cin >> num;
-    attackSelf = static_cast<RuleAttackSelf>(num);
+        << "0: 禁止\n"
+        << "1: 可能\n";
+    do {
+        std::cerr << "> ";
+        std::cin >> num;
+        switch (num) {
+        case 0:
+            prefix += "no-self-attack";
+            attackSelf = RuleAttackSelf::forbid;
+            break;
+        case 1:
+            prefix += "self-attack";
+            attackSelf = RuleAttackSelf::allow;
+            break;
+        default:
+            num = -1;
+            break;
+        }
+    } while (num == -1);
 
-    std::cout
+
+    // 描画設定を入力
+    std::cerr
         << "\n"
         << "[描画設定]\n"
         << "* 千日手グラフのループ\n"
         << "0: ループしない\n"
-        << "1: ループする(大きな状態遷移図では時間がかかるかもしれません)\n"
-        << "> ";
-    std::cin >> num;
-    graphLoop = static_cast<GraphLoop>(num);
-    std::cout
+        << "1: ループする (大きな状態遷移図では時間がかかるかもしれません)\n";
+
+    do {
+        std::cerr << "> ";
+        std::cin >> num;
+        switch (num) {
+        case 0:
+            suffix += "no-loop";
+            graphLoop = GraphLoop::forbid;
+            break;
+        case 1:
+            suffix += "loop";
+            graphLoop = GraphLoop::allow;
+            break;
+        default:
+            num = -1;
+            break;
+        }
+    } while (num == -1);
+    suffix += "_";
+
+    std::cerr
         << "\n"
         << "* 千日手から外れる遷移\n"
-        << "0: 描画する\n"
-        << "1: 描画しない\n"
-        << "> ";
-    std::cin >> num;
-    graphShowLose = static_cast<GraphShowLose>(num);
+        << "0: 描画しない\n"
+        << "1: 描画する\n";
+    do {
+        std::cerr << "> ";
+        std::cin >> num;
+        switch (num) {
+        case 0:
+            suffix += "hide-out-trans";
+            graphShowLose = GraphShowLose::hide;
+            break;
+        case 1:
+            suffix += "show-out-trans";
+            graphShowLose = GraphShowLose::show;
+            break;
+        default:
+            num = -1;
+        }
+    } while (num == -1);
 
-    // ファイル名のプレフィックス
-    std::stringstream pss;
-    pss << max << "_";
-    switch (overflow) {
-    case RuleOverflow::mod:
-        pss << "mod";
-        break;
-    case RuleOverflow::death:
-        pss << "no-mod";
-        break;
-    }
-    pss << "_";
-    switch (bunshin) {
-    case RuleBunshin::allow:
-        pss << "split";
-        break;
-    case RuleBunshin::resuscitateOnly:
-        pss << "resuscitation-only-split";
-        break;
-    case RuleBunshin::forbid:
-        pss << "no-split";
-        break;
-    }
-    pss << "_";
-    switch (attackSelf) {
-    case RuleAttackSelf::allow:
-        pss << "self-attack";
-        break;
-    case RuleAttackSelf::forbid:
-        pss << "no-self-attack";
-        break;
-    }
-    std::string prefix = pss.str();
-
-    std::stringstream sss;
-    switch (graphLoop) {
-    case GraphLoop::allow:
-        sss << "loop";
-        break;
-    case GraphLoop::forbid:
-        sss << "no-loop";
-        break;
-    }
-    sss << "_";
-    switch (graphShowLose) {
-    case GraphShowLose::show:
-        sss << "show-out-trans";
-        break;
-    case GraphShowLose::hide:
-        sss << "hide-out-trans";
-        break;
-    }
-    std::string suffix = sss.str();
-
-
-    std::cout << "[初期化]" << std::endl;
+    std::cerr << "[ゲーム探索]" << std::endl;
     GameSolver gameSolver(GameRule(max, overflow, bunshin, attackSelf));
-    std::cout << "終了" << std::endl;
+    std::cerr << "完了" << std::endl;
 
-    std::ofstream fout;
+    std::cerr << "[グラフ描画]" << std::endl;
 
-    std::cout << "[first-win-graph]" << std::endl;
-    fout.open("dot/" + prefix + "_first.dot");
-    if (fout.fail()) {
-        std::cout << "ファイルオープンに失敗しました" << std::endl;
-        return 1;
-    }
-    gameSolver.makeWinGameGraph(0, fout);
-    fout.close();
+    std::string directory = "./dot/";
 
-    std::cout << "[second-win-graph]" << std::endl;
-    fout.open("dot/" + prefix + "_second.dot");
-    if (fout.fail()) {
-        std::cout << "ファイルオープンに失敗しました" << std::endl;
-        return 1;
-    }
-    gameSolver.makeWinGameGraph(1, fout);
-    fout.close();
-
-    std::cout << "[loop-graph]" << std::endl;
-    fout.open("dot/" + prefix + "_loop_" + suffix + ".dot");
-    if (fout.fail()) {
-        std::cout << "ファイルオープンに失敗しました" << std::endl;
-        return 1;
-    }
-    gameSolver.makeLoopGameGraph(fout, graphLoop, graphShowLose);
-    fout.close();
+    fileOutput(directory + prefix + "_first.dot", [&](std::ostream &out){gameSolver.makeWinGameGraph(0, out);});
+    fileOutput(directory + prefix + "_second.dot", [&](std::ostream &out){gameSolver.makeWinGameGraph(1, out);});
+    fileOutput(directory + prefix + "_draw_" + suffix + ".dot", [&](std::ostream &out){gameSolver.makeLoopGameGraph(out, graphLoop, graphShowLose);});
 
     return 0;
+}
+
+
+void fileOutput(std::string fileName, std::function<void(std::ostream &)> toStream) {
+    std::ofstream fout;
+    fout.open(fileName);
+    if (fout.fail()) {
+        std::cerr << "ファイループンに失敗しました: " << fileName << std::endl;
+    } else {
+        toStream(fout);
+        fout.close();
+        std::cout << fileName << std::endl;
+    }
 }
